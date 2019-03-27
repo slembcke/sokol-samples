808d73fe0adcba3cc247dd439d8f84865a75def2
slembcke <slembcke@gmail.com>
2019-03-26 20:54:38 -0500

    Some initial cleanup.

--- glfw/quad-glfw.c
+++ glfw/quad-glfw.c
@@ -9,113 +9,95 @@
 #define SOKOL_GLCORE33
 #include "sokol_gfx.h"
 
-int main() {
+#define GLSL33(x) "#version 330\n" #x
 
-    const int WIDTH = 640;
-    const int HEIGHT = 480;
+int main(){
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* w = glfwCreateWindow(640, 480, "Sokol Quad GLFW", 0, 0);
+	glfwMakeContextCurrent(w);
+	glfwSwapInterval(1);
+	flextInit();
 
-    /* create window and GL context via GLFW */
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* w = glfwCreateWindow(WIDTH, HEIGHT, "Sokol Quad GLFW", 0, 0);
-    glfwMakeContextCurrent(w);
-    glfwSwapInterval(1);
-    flextInit();
+	sg_desc desc = {};
+	sg_setup(&desc);
+	assert(sg_isvalid());
 
-    /* setup sokol_gfx */
-    sg_desc desc = {0};
-    sg_setup(&desc);
-    assert(sg_isvalid());
+	float vertices[] = {
+		-0.5f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 0.0f, 1.0f,
+	};
+	
+	sg_buffer vbuf = sg_make_buffer(&(sg_buffer_desc){
+		.size = sizeof(vertices),
+		.content = vertices,
+	});
 
-    /* create a vertex buffer */
-    float vertices[] = {
-        // positions            colors
-        -0.5f,  0.5f, 0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
-         0.5f,  0.5f, 0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
-         0.5f, -0.5f, 0.5f,     0.0f, 0.0f, 1.0f, 1.0f,
-        -0.5f, -0.5f, 0.5f,     1.0f, 1.0f, 0.0f, 1.0f,        
-    };
-    sg_buffer_desc vbuf_desc = {
-        .size = sizeof(vertices),
-        .content = vertices,
-    };
-    sg_buffer vbuf = sg_make_buffer(&vbuf_desc);
+	uint16_t indices[] = {0, 1, 2, 0, 2, 3};
+	
+	sg_buffer ibuf = sg_make_buffer(&(sg_buffer_desc){
+		.size = sizeof(indices),
+		.type = SG_BUFFERTYPE_INDEXBUFFER,
+		.content = indices,
+	});
 
-    /* create an index buffer */
-    uint16_t indices[] = {
-        0, 1, 2,    // first triangle
-        0, 2, 3,    // second triangle        
-    };
-    sg_buffer_desc ibuf_desc = {
-        .size = sizeof(indices),
-        .type = SG_BUFFERTYPE_INDEXBUFFER,
-        .content = indices,
-    };
-    sg_buffer ibuf = sg_make_buffer(&ibuf_desc);
+	sg_bindings bind = {
+		.vertex_buffers[0] = vbuf,
+		.index_buffer = ibuf
+	};
 
-    /* define the resource bindings */
-    sg_bindings bind = {
-        .vertex_buffers[0] = vbuf,
-        .index_buffer = ibuf
-    };
+	sg_shader shd = sg_make_shader(&(sg_shader_desc){
+		.vs.source = GLSL33(
+			layout(location=0) in vec4 position;
+			layout(location=1) in vec4 color0;
+			out vec4 color;
+			void main() {
+				gl_Position = position;
+				color = color0;
+			}
+		),
+		.fs.source = GLSL33(
+			in vec4 color;
+			out vec4 frag_color;
+			void main() {
+				frag_color = color;
+			}
+		),
+	});
+	
+	sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
+		.shader = shd,
+		.index_type = SG_INDEXTYPE_UINT16,
+		.layout = {
+			.attrs = {
+				[0] = { .offset=0, .format=SG_VERTEXFORMAT_FLOAT3 },
+				[1] = { .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 }
+			}
+		}
+	});
 
-    /* create a shader (use vertex attribute locations) */
-    sg_shader shd = sg_make_shader(&(sg_shader_desc){
-        .vs.source =
-            "#version 330\n"
-            "layout(location=0) in vec4 position;\n"
-            "layout(location=1) in vec4 color0;\n"
-            "out vec4 color;\n"
-            "void main() {\n"
-            "  gl_Position = position;\n"
-            "  color = color0;\n"
-            "}\n",
-        .fs.source =
-            "#version 330\n"
-            "in vec4 color;\n"
-            "out vec4 frag_color;\n"
-            "void main() {\n"
-            "  frag_color = color;\n"
-            "}\n"
-    });
-    
-    /* create a pipeline object (default render state is fine) */
-    sg_pipeline pip = sg_make_pipeline(&(sg_pipeline_desc){
-        .shader = shd,
-        .index_type = SG_INDEXTYPE_UINT16,
-        .layout = {
-            /* test to provide attr offsets, but no buffer stride, this should compute the stride */
-            .attrs = {
-                /* vertex attrs can also be bound by location instead of name (but not in GLES2) */
-                [0] = { .offset=0, .format=SG_VERTEXFORMAT_FLOAT3 },
-                [1] = { .offset=12, .format=SG_VERTEXFORMAT_FLOAT4 }
-            }
-        }
-    });
+	sg_pass_action pass_action = {};
 
-    /* default pass action */
-    sg_pass_action pass_action = { 0 };
+	while (!glfwWindowShouldClose(w)) {
+		int cur_width, cur_height;
+		glfwGetFramebufferSize(w, &cur_width, &cur_height);
+		sg_begin_default_pass(&pass_action, cur_width, cur_height);
+		sg_apply_pipeline(pip);
+		sg_apply_bindings(&bind);
+		sg_draw(0, 6, 1);
+		sg_end_pass();
+		sg_commit();
+		glfwSwapBuffers(w);
+		glfwPollEvents();
+	}
 
-    /* draw loop */
-    while (!glfwWindowShouldClose(w)) {
-        int cur_width, cur_height;
-        glfwGetFramebufferSize(w, &cur_width, &cur_height);
-        sg_begin_default_pass(&pass_action, cur_width, cur_height);
-        sg_apply_pipeline(pip);
-        sg_apply_bindings(&bind);
-        sg_draw(0, 6, 1);
-        sg_end_pass();
-        sg_commit();
-        glfwSwapBuffers(w);
-        glfwPollEvents();
-    }
+	sg_shutdown();
+	glfwTerminate();
 
-    /* cleanup */
-    sg_shutdown();
-    glfwTerminate();
-
-    return 0;
+	return 0;
 }
